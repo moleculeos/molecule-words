677d63dd42ca56dae3efe7f8355dee7346bff5a7
Michal Wartacz <michalecznik123@gmail.com>
2022-04-09 19:29:19 +0200

    add json web token auth

--- backend/moleculewords/views.py
+++ backend/moleculewords/views.py
@@ -6,13 +6,13 @@ from .models import Language, Word
 
 class LanguageViewSet(viewsets.ModelViewSet):
     serializer_class = LanguageSerializer
-    #permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     queryset = Language.objects.all()
 
 
 class WordViewSet(viewsets.ModelViewSet):
     serializer_class = WordSerializer
-    #permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return Word.objects.filter(language__name=self.request.GET.get('language', default=''))
