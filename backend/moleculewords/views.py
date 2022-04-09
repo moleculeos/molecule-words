e787ee8d4188519389fa4f57beda7d5c2cb1c2fe
Michal Wartacz <michalecznik123@gmail.com>
2022-04-09 19:24:07 +0200

    write viewset for Word and Language

--- backend/moleculewords/views.py
+++ backend/moleculewords/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .serializers import WordSerializer, LanguageSerializer
+from .models import Language, Word
 
-# Create your views here.
+
+class LanguageViewSet(viewsets.ModelViewSet):
+    serializer_class = LanguageSerializer
+    #permission_classes = [IsAuthenticated]
+    queryset = Language.objects.all()
+
+
+class WordViewSet(viewsets.ModelViewSet):
+    serializer_class = WordSerializer
+    #permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Word.objects.filter(language__name=self.request.GET.get('language', default=''))
+
