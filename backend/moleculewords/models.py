11b5368aa34b697591a8a499389761a7a715cf91
Michal Wartacz <michalecznik123@gmail.com>
2022-04-09 18:11:02 +0200

    write Language and Word model

--- backend/moleculewords/models.py
+++ backend/moleculewords/models.py
@@ -1,3 +1,36 @@
+import re
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Language(models.Model):
+    name = models.CharField(max_length=50)
+
+    @property
+    def words_amount(self):
+        return len(self.words)
+
+    @property
+    def good_words_amount(self):
+        return self.words.filter(known_level=2)
+    
+    @property
+    def medium_words_amount(self):
+        return self.words.filter(known_level=1)
+
+    @property
+    def almost_words_amount(self):
+        return self.words.filter(known_level=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Word(models.Model):
+    language = models.ForeignKey(Language, related_name='words', on_delete=models.CASCADE)
+    word = models.CharField(max_length=200)
+    translations = models.TextField()
+    known_level = models.IntegerField(validators=[MaxValueValidator(2), MinValueValidator(0)])
+
+    def __str__(self):
+        return self.word
